423ecc495c97416a697e5a03a8b20ec203162b0e
aklema094 <aklemasultana094@gmail.com>
2024-12-23 08:34:26 +0600

    add new student

--- GradingSystem.java
+++ GradingSystem.java
@@ -1,8 +1,11 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class GradingSystem{
+    private static HashMap<Integer, String> studentRecords = new HashMap<>();
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
 
        while (true) {
         mainMenu();
@@ -11,7 +14,12 @@ public class GradingSystem{
 
         switch (choice) {
             case 1:
-                //addStudent();
+              System.out.print("Enter Id : ");
+              int id = scanner.nextInt();
+              scanner.nextLine();
+              System.out.print("Enter name : ");
+              String name = scanner.nextLine();
+              addStudent(id,name);
                 break;
             case 2:
                 //updateStudent();
@@ -42,5 +50,19 @@ public class GradingSystem{
     public static void mainMenu(){
         System.out.println("1. Add Student \n2. Update Student \n3. Delete Student \n4. Search Student");
         System.out.println("5. Update Mark \n6. Display Grade Distribution \n7. Exit");
+    }
+
+    // add new student 
+    public static void addStudent(int id, String name){
+
+        if(studentRecords.containsKey(id)){
+            System.out.println("Student with this ID already exist");
+            return;
+        }
+        
+       studentRecords.put(id, name);
+       System.out.println("Student added successfully");
+
+
     }
 }
